c70f23f330a1c9d343466d0f0d3f2dbb6bd1bcfd
Mrhardfart <mrhardfart7889@gmail.com>
2026-06-07 13:06:35 +0200

    Add mainLoader.lua to load scripts based on PlaceId

---
+++ mainLoader.lua
@@ -0,0 +1,5 @@
+repeat wait() until game:IsLoaded()
+
+if game.PlaceId == 130247632398296 or game.PlaceId == 73277298613633 then -- afse
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Mrhardfart/Rublax/refs/heads/main/afse.lua"))()
+end
