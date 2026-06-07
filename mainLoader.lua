b2fcc2ec8272e1aa1d04617f78180c4d0f8ab4d8
Mrhardfart <mrhardfart7889@gmail.com>
2026-06-07 14:17:54 +0200

    Update mainLoader.lua

--- mainLoader.lua
+++ mainLoader.lua
@@ -2,6 +2,6 @@ repeat wait() until game:IsLoaded()
 
 if game.PlaceId == 130247632398296 or game.PlaceId == 73277298613633 then -- afse
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Mrhardfart/Rublax/refs/heads/main/afse.lua"))()
-elseif game.PlaceId == 119172704194357 then
+elseif game.PlaceId == 119172704194357 then -- guess my word
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Mrhardfart/Rublax/refs/heads/main/gmw.lua"))()
 end
