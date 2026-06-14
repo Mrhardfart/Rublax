9f057d00185ac5d327508ba688f27abe78f639fc
Mrhardfart <mrhardfart7889@gmail.com>
2026-06-14 10:51:54 +0200

    Update mainLoader.lua

--- mainLoader.lua
+++ mainLoader.lua
@@ -4,4 +4,6 @@ if game.PlaceId == 130247632398296 or game.PlaceId == 73277298613633 then -- afs
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Mrhardfart/Rublax/refs/heads/main/afse.lua"))()
 elseif game.PlaceId == 119172704194357 then -- guess my word
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Mrhardfart/Rublax/refs/heads/main/gmw.lua"))()
+elseif game.PlaceId == 100429474155186 then -- new afs
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Mrhardfart/Rublax/refs/heads/main/newafs.lua"))()
 end
